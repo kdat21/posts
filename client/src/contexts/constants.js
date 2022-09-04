681b568b98931083619f17bb91fe23e7c8942b13
kdat21 <buikhacdat1@gmail.com>
2022-09-04 23:25:15 +0700

    Prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://radiant-dawn-99368.herokuapp.com/api"
-    : "somedeployedURL";
+    ? "http://localhost:5000/api"
+    : "https://gentle-shore-34483.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 
